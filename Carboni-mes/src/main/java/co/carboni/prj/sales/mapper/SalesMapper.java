6662293aa33a4ea9538820c698c62fd74535ecd3
KYM <kym8439@gmail.com>
2022-07-11 19:14:40 +0900

    07-11 김앵미 학원커밋

---
+++ Carboni-mes/src/main/java/co/carboni/prj/sales/mapper/SalesMapper.java
@@ -0,0 +1,5 @@
+package co.carboni.prj.sales.mapper;
+
+public interface SalesMapper {
+
+}
---
+++ Carboni-mes/src/main/java/co/carboni/prj/sales/service/SalesServiceImpl.java
@@ -0,0 +1,5 @@
+package co.carboni.prj.sales.service;
+
+public class SalesServiceImpl implements SalesService {
+
+}
