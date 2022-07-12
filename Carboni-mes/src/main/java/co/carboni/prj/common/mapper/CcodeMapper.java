b0191d3f6f3aef89a666a8818a4fb4eb3ed916a5
mistlud <sadsnow32@gmail.com>
2022-07-12 17:45:24 +0900

    common

--- Carboni-mes/src/main/java/co/carboni/prj/common/mapper/CcodeMapper.java
+++ Carboni-mes/src/main/java/co/carboni/prj/common/mapper/CcodeMapper.java
@@ -1,5 +1,19 @@
 package co.carboni.prj.common.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import co.carboni.prj.common.vo.CcodeVO;
+
 public interface CcodeMapper {
+
+	List<CcodeVO> selectCodeAll();
+	CcodeVO selectCodeClass(CcodeVO vo);
+	int codeInsertBig(CcodeVO vo);
+	int codeInsertSmall(CcodeVO vo);
+	int codeDelete(CcodeVO vo);
+	int codeUpdate(CcodeVO vo);
 
 }
