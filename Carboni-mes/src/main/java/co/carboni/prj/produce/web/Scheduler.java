9c47e3c4ec14ddc46ee681c1f2046f77a0e25b71
haddress <hyunju9366@gmail.com>
2022-08-01 12:53:42 +0900

    서버 구축 확인

--- Carboni-mes/src/main/java/co/carboni/prj/produce/web/Scheduler.java
+++ Carboni-mes/src/main/java/co/carboni/prj/produce/web/Scheduler.java
@@ -15,17 +15,15 @@ public class Scheduler {
 	@Autowired
 	ProdService service;
 	
-	@Scheduled(fixedRate = 5000)
-	public void schedule() {
-		
-		 service.schedule(null, "SS-1");
-		 service.schedule("SS-1", "SS-5");
-		 service.schedule("SS-5", "SS-7");
-		 service.schedule(null, "SS-2");
-		 service.schedule("SS-2", "SS-6");
-		 service.schedule("SS-6", "SS-7");
-		 
-		
-	}
+	/*
+	 * @Scheduled(fixedRate = 5000) public void schedule() {
+	 * 
+	 * service.schedule(null, "SS-1"); service.schedule("SS-1", "SS-5");
+	 * service.schedule("SS-5", "SS-7"); service.schedule(null, "SS-2");
+	 * service.schedule("SS-2", "SS-6"); service.schedule("SS-6", "SS-7");
+	 * 
+	 * 
+	 * }
+	 */
 	
 }
