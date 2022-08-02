245dbc94925e7cd6223374b6082504bae6a32b42
haddress <hyunju9366@gmail.com>
2022-08-02 18:12:46 +0900

    긴급중지, 재시작, 엑셀, 라인검색 필요

--- Carboni-mes/src/main/java/co/carboni/prj/produce/web/Scheduler.java
+++ Carboni-mes/src/main/java/co/carboni/prj/produce/web/Scheduler.java
@@ -1,19 +1,34 @@
 package co.carboni.prj.produce.web;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import co.carboni.prj.produce.service.ProdService;
 
-@EnableScheduling
 @Component
-public class Scheduler {
+public class Scheduler extends Thread {
 
 	
 	@Autowired
 	ProdService service;
+	
+	
+   @Override
+   public void run() {
+      List<String> list = Arrays.asList(null, "SS-1","SS-5","SS-7");
+      for(int i=1 ; i<list.size(); i++) {
+         service.schedule(list.get(i-1), list.get(i));
+         try {
+            Thread.sleep(8000);
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+         }
+      }
+   }
+
 	
 	/*
 	 * @Scheduled(fixedRate = 5000) public void schedule() {
@@ -25,5 +40,7 @@ public class Scheduler {
 	 * 
 	 * }
 	 */
+	
+	
 	
 }
